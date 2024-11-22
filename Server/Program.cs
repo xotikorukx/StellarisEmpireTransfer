bb6ff4c5559b26488efacad3aa256d4fcfb22cd1
Daemon Brooks <anomalouslysingular@gmail.com>
2024-11-22 08:55:41 -0600

    Remove unnecessary imports

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 using CustomEmpireManager = EmpireManager.EmpireManager;
 
 namespace Server
--- StellarisEmpireTransferClient/Program.cs
+++ StellarisEmpireTransferClient/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net.Sockets;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Net.Sockets;
 using System.Configuration;
 using CustomEmpireManager = EmpireManager.EmpireManager;
 
